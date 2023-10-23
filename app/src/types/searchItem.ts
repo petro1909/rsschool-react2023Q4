dc1bd12fa308d814e587499bc3a510eadac74073
petro1909 <petya-97_97@mail.ru>
2023-10-23 16:09:39 +0300

    feat: add types for search items

---
+++ app/src/types/searchItem.ts
@@ -0,0 +1,18 @@
+export type SearchItem = {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+};
+export type SearchItems = {
+  items: Array<SearchItem>;
+};
+export type SearchItemJsonResult = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Array<SearchItem>;
+};
