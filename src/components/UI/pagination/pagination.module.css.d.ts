adb58d0252a46994bb54cb5b68e4cf6250addee1
petro1909 <petya-97_97@mail.ru>
2023-11-05 13:36:55 +0300

    refactor: remove unused files

--- src/components/UI/pagination/pagination.module.css.d.ts
+++
@@ -1,7 +0,0 @@
-declare const classNames: {
-  readonly pages: 'pages';
-  readonly page: 'page';
-  readonly pageCurrent: 'pageCurrent';
-  readonly pageDisabled: 'pageDisabled';
-};
-export = classNames;
--- src/components/UI/pagination/paginationProps.ts
+++
@@ -1,6 +0,0 @@
-import { TVShowResultsConfig } from '../../../types/api/apiResults';
-import { SearchFnProps } from '../../../types/searchFunctionProps';
-
-export type PaginationProps = SearchFnProps & {
-  resultsConfig: TVShowResultsConfig;
-};
