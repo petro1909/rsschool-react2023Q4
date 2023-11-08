b3ee907c8e05fe721baadf64608c37ad5fc59f34
petro1909 <petya-97_97@mail.ru>
2023-11-08 21:07:57 +0300

    feat: create Searchbar context

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,12 +1,14 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import { getValueByKeyFromLocalStorage } from '@service/storageService';
 import { CustomForm } from '@components/UI/customForm/customForm';
 import searchIcon from '@assets/search.svg';
 import { useTVShowsSearchParams } from '@hooks/useTVShowsSearchParams';
 import classNames from './searchBar.module.css';
+import { SearchBarContext } from '@page/main/main';
 
 export const SearchBar = () => {
+  const { setSearchTerm } = useContext(SearchBarContext);
   const [text, setText] = useState('');
   const updateTVShowsParams = useTVShowsSearchParams();
 
@@ -16,12 +18,14 @@ export const SearchBar = () => {
   useEffect(() => {
     const currentSearchTerm = getValueByKeyFromLocalStorage();
     setText(currentSearchTerm);
+    setSearchTerm(currentSearchTerm);
   }, []);
 
   const search = () => {
     const pageSizeStr = Number(queryParams.get('pageSize'));
     const pageSize = !isNaN(pageSizeStr) ? pageSizeStr : 10;
     updateTVShowsParams({ searchTerm: text, page: 1, pageSize: pageSize });
+    setSearchTerm(text);
   };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
--- src/page/main/main.tsx
+++ src/page/main/main.tsx
@@ -3,12 +3,25 @@ import { ErrorButton } from '@components/error/errorButton/errorButton';
 import { SearchBar } from '@components/searchBar/searchBar';
 import { TVShows } from '@components/tvShow/tvShows/tvShows';
 import classNames from './main.module.css';
+import { createContext, useState } from 'react';
+
+export const SearchBarContext = createContext<{
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+}>({ setSearchTerm: () => {} });
 
 export default function Main() {
+  const [, setSearchTerm] = useState('');
   return (
     <>
       <header className={classNames.header}>
-        <SearchBar />
+        <SearchBarContext.Provider
+          value={{
+            setSearchTerm,
+          }}
+        >
+          <SearchBar />
+        </SearchBarContext.Provider>
+
         <ErrorButton />
       </header>
       <main>
