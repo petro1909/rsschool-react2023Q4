07cf8d7537cf6e33530713d890d4deec144fb788
petro1909 <petya-97_97@mail.ru>
2023-11-11 20:20:13 +0300

    feat: add tests for pagination component

---
+++ src/components/UI/itemsControl/pagination/pagination.test.tsx
@@ -0,0 +1,19 @@
+import { TVShowResultsConfig } from '@app_types/api/apiResults';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
+import { Pagination } from './pagination';
+
+describe('Pagination component', () => {
+  it('should update search page parameter if page is changed', () => {
+    const config: TVShowResultsConfig = { totalCount: 50, currentPage: 1, pageSize: 10 };
+    render(<Pagination resultsConfig={config} />, { wrapper: BrowserRouter });
+    const secondPageButton = screen.getByTestId('2');
+    fireEvent.click(secondPageButton);
+
+    const searchParams = new URLSearchParams(window.location.search);
+    const pageNumber = searchParams.get('page');
+
+    expect(pageNumber).toEqual('2');
+  });
+});
