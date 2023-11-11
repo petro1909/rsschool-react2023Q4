fa019bf2d7a4ba47542f98d9f6efecc04039708e
petro1909 <petya-97_97@mail.ru>
2023-11-11 19:36:28 +0300

    feat: update tests setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom';
+import { cleanup } from '@testing-library/react';
+import { afterEach } from 'vitest';
+
+afterEach(() => {
+  localStorage.clear();
+  cleanup();
+});
