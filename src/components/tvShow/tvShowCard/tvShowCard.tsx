e679e599a955b6c691a2e02f3d2d6d729deedb66
petro1909 <petya-97_97@mail.ru>
2023-11-11 21:03:31 +0300

    refactor: add data-testid

--- src/components/tvShow/tvShowCard/tvShowCard.tsx
+++ src/components/tvShow/tvShowCard/tvShowCard.tsx
@@ -17,38 +17,42 @@ export function TVShowCard({ item }: CardProps) {
   };
 
   return (
-    <section className={classNames.itemWrapper} onClick={openExtendedShowCard}>
-      <div className={classNames.itemImage}>
-        <img src={item.image}></img>
-      </div>
-      <div className={classNames.itemInfo}>
-        <section>
-          <h3 className={classNames.itemHeader}>{item.title}</h3>
-          <h3 className={classNames.itemHeader}>{item.titleOriginal}</h3>
-        </section>
-        <hr />
-        <section>
-          <div className={classNames.itemPropertyWrapper}>
-            <span className={classNames.itemPropertyName}>Rating: </span>
-            <span className={classNames.itemPropertyValue}>{item.rating}</span>
-            <span className={classNames.itemPropertyValue}>(Votes: {item.voted} )</span>
-          </div>
-        </section>
-        <section>
-          <div className={classNames.itemPropertyWrapper}>
-            <span className={classNames.itemPropertyName}>Status: </span>
-            <span className={classNames.itemPropertyValue}>{item.status}</span>
-          </div>
-          <div className={classNames.itemPropertyWrapper}>
-            <span className={classNames.itemPropertyName}>Total Seasons: </span>
-            <span className={classNames.itemPropertyValue}>{item.totalSeasons}</span>
-          </div>
-          <div className={classNames.itemPropertyWrapper}>
-            <span className={classNames.itemPropertyName}>Year: </span>
-            <span className={classNames.itemPropertyValue}>{item.year}</span>
-          </div>
-        </section>
-      </div>
+    <section data-testid={'tvShowCardElement'} onClick={openExtendedShowCard}>
+      <section className={classNames.itemWrapper} data-testid={item.id}>
+        <div className={classNames.itemImage}>
+          <img src={item.image}></img>
+        </div>
+        <div className={classNames.itemInfo}>
+          <section>
+            <h3 className={classNames.itemHeader} data-testid={item.title}>
+              {item.title}
+            </h3>
+            <h3 className={classNames.itemHeader}>{item.titleOriginal}</h3>
+          </section>
+          <hr />
+          <section>
+            <div className={classNames.itemPropertyWrapper}>
+              <span className={classNames.itemPropertyName}>Rating: </span>
+              <span className={classNames.itemPropertyValue}>{item.rating}</span>
+              <span className={classNames.itemPropertyValue}>(Votes: {item.voted} )</span>
+            </div>
+          </section>
+          <section>
+            <div className={classNames.itemPropertyWrapper}>
+              <span className={classNames.itemPropertyName}>Status: </span>
+              <span className={classNames.itemPropertyValue}>{item.status}</span>
+            </div>
+            <div className={classNames.itemPropertyWrapper}>
+              <span className={classNames.itemPropertyName}>Total Seasons: </span>
+              <span className={classNames.itemPropertyValue}>{item.totalSeasons}</span>
+            </div>
+            <div className={classNames.itemPropertyWrapper}>
+              <span className={classNames.itemPropertyName}>Year: </span>
+              <span className={classNames.itemPropertyValue}>{item.year}</span>
+            </div>
+          </section>
+        </div>
+      </section>
     </section>
   );
 }
