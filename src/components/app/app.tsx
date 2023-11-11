7262aa46f178a670618a8e417b015aa89406219f
petro1909 <petya-97_97@mail.ru>
2023-11-11 19:37:33 +0300

    refactor: move BrowserRouter to main.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Main from '@page/main/main';
 import { About } from '@page/about/about';
 import { NotFound } from '@page/notFound/notFound';
@@ -6,14 +6,13 @@ import { TVShowExtended } from '@components/tvShow/tvShowExtended/tvShowExtended
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}>
-          <Route path="/" element={<TVShowExtended />} />
-        </Route>
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />}>
+        <Route path="/" element={<TVShowExtended />} />
+      </Route>
+      <Route path="/about" element={<About />} />
+      <Route path="/notFound" element={<NotFound />} />
+      <Route path="*" element={<Navigate to="/notFound" />} />
+    </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/app.js';
 import { ErrorBoundary } from './components/error/errorBoundary/errorBoundary.js';
 import './style.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ErrorBoundary>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ErrorBoundary>
 );
