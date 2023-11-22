27dfc35f0ea74c5b4638cc44c76876af4107d793
petro1909 <petya-97_97@mail.ru>
2023-11-22 18:42:42 +0300

    refactor: change == to ===

--- src/components/UI/itemsControl/pagination/pagination.tsx
+++ src/components/UI/itemsControl/pagination/pagination.tsx
@@ -14,7 +14,7 @@ export function Pagination({ resultsConfig, submit }: PaginationProps) {
   return (
     <section className={classNames.pages}>
       <PaginationButton
-        disabled={resultsConfig.currentPage == 1}
+        disabled={resultsConfig.currentPage === 1}
         onClick={submit}
         pageNumber={resultsConfig.currentPage - 1}
         key={'prev'}
@@ -22,24 +22,24 @@ export function Pagination({ resultsConfig, submit }: PaginationProps) {
         &#x2BC7;
       </PaginationButton>
       {pageArray.map((page, index) =>
-        page == 0 ? (
+        page === 0 ? (
           <span key={index + 0.5} className={classNames.pageDots}>
             ...
           </span>
         ) : (
           <PaginationButton
             key={page}
-            disabled={resultsConfig.currentPage == page}
+            disabled={resultsConfig.currentPage === page}
             onClick={submit}
             pageNumber={page}
-            current={resultsConfig.currentPage == page}
+            current={resultsConfig.currentPage === page}
           >
             {page}
           </PaginationButton>
         )
       )}
       <PaginationButton
-        disabled={resultsConfig.currentPage == pagesCount}
+        disabled={resultsConfig.currentPage === pagesCount}
         onClick={submit}
         pageNumber={resultsConfig.currentPage + 1}
         key={'next'}
