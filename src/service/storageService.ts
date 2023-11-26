1ea1293b25d86b8afb6c9221972171336cff1dc4
petro1909 <petya-97_97@mail.ru>
2023-11-26 18:47:30 +0300

    feat: update getValueFromLocalStorage function

--- src/service/storageService.ts
+++ src/service/storageService.ts
@@ -1,9 +1,9 @@
 export const storageKey = 'searchValue';
 
-export function getValueByKeyFromLocalStorage(): string {
-  return localStorage.getItem(storageKey) || '';
+export function getValueFromLocalStorage(): string {
+  return typeof window !== 'undefined' ? localStorage.getItem(storageKey) || '' : '';
 }
 
-export function setValueByKeyToLocalStorage(value: string): void {
+export function setValueToLocalStorage(value: string): void {
   localStorage.setItem(storageKey, value);
 }
