1d3ff2de1376f78e5ffab5e95393075c8754b154
petro1909 <petya-97_97@mail.ru>
2023-11-26 21:41:24 +0300

    refactor: update vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'v8',
+      all: true,
+      reporter: 'text',
+      include: ['src/*'],
+      exclude: ['src/types/*'],
+    },
+  },
+});
