75d14e2ad177298784c4b1c0085764c6117704f0
petro1909 <petya-97_97@mail.ru>
2023-11-26 21:43:34 +0300

    refactorL update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
+    setupFiles: ['./test/setupTests.ts'],
     coverage: {
       provider: 'v8',
       all: true,
