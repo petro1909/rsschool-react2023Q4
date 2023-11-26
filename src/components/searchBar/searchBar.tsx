0756bfa1c336d36d18e3c12b1ca4c8210dcb0468
petro1909 <petya-97_97@mail.ru>
2023-11-26 18:18:57 +0300

    feat: update searchBar component

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,35 +1,35 @@
-import { ChangeEvent, useState } from 'react';
-import { CustomForm } from '@components/UI/customForm/customForm';
-import searchIcon from '@assets/search.svg';
+import { FormEvent, useEffect, useState } from 'react';
+import searchIcon from '../../public/search.svg';
 import classNames from './searchBar.module.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../../store/store';
-import { setSearchTerm } from '../../store/tvShowsSlice';
-import { useSearchParams } from 'react-router-dom';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { getValueFromLocalStorage, setValueToLocalStorage } from '@service/storageService';
+
 export const SearchBar = () => {
-  const searchTerm = useSelector((state: RootState) => state.tvShows.searchTerm);
-  const [text, setText] = useState(searchTerm);
-  const dispatch = useDispatch();
+  const [text, setText] = useState('');
+  const router = useRouter();
 
-  const [, setQueryParams] = useSearchParams();
-  const search = () => {
-    setQueryParams((params) => {
-      params.set('page', `${1}`);
-      return params;
-    });
-    dispatch(setSearchTerm(text));
-  };
+  useEffect(() => {
+    setText(getValueFromLocalStorage());
+  }, []);
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setValueToLocalStorage(text);
+    router.push({ query: { ...router.query, searchQuery: text } });
   };
 
   return (
-    <CustomForm
-      inputProps={{ type: 'text', placeholder: 'search', value: text, change: handleInputChange }}
-      submitProps={{ submitFn: search }}
-    >
-      <img src={searchIcon} className={classNames.searchImage}></img>
-    </CustomForm>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">
+        <Image src={searchIcon} alt="search" className={classNames.image} />
+      </button>
+    </form>
   );
 };
