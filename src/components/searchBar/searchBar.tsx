ce9594c1d24869bbdfc9535ba56e16ba00408aad
petro1909 <petya-97_97@mail.ru>
2023-11-26 21:45:46 +0300

    feat: change localstorage to query params upon mounting stage

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -3,32 +3,33 @@ import searchIcon from '../../public/search.svg';
 import classNames from './searchBar.module.css';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
-import { getValueFromLocalStorage, setValueToLocalStorage } from '@service/storageService';
+import { getTVshowsQueryParams } from '@service/queryParamsService';
 
 export const SearchBar = () => {
   const [text, setText] = useState('');
   const router = useRouter();
 
   useEffect(() => {
-    setText(getValueFromLocalStorage());
+    const { searchQuery } = getTVshowsQueryParams(router.query);
+    setText(searchQuery);
   }, []);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setValueToLocalStorage(text);
     router.push({ query: { ...router.query, searchQuery: text, page: 1 } });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
+        data-testId="input"
         type="text"
         placeholder="search"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button type="submit">
-        <Image src={searchIcon} alt="search" className={classNames.image} />
+      <button type="submit" data-testId="submit">
+        <Image src={searchIcon} alt="search" className={classNames.image} width={50} height={50} />
       </button>
     </form>
   );
