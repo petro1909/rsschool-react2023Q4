928113803bc92d70e77c7a8bf739ae4cafaf1fb0
petro1909 <petya-97_97@mail.ru>
2023-12-02 15:40:36 +0300

    init: init project config

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types='vite/client'/>
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@app_types': path.resolve(__dirname, './src/types'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+    },
+  },
+});
