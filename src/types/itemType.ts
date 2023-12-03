5b4872510bdd06dd641d912aa65579d244c7c260
petro1909 <petya-97_97@mail.ru>
2023-12-04 01:58:27 +0300

    feat: create app type

---
+++ src/types/itemType.ts
@@ -0,0 +1,10 @@
+export type ItemType = {
+  id?: number;
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  gender: string;
+  picture: string;
+  country: string;
+};
---
+++ src/types/passwordStrengthLevel.ts
@@ -0,0 +1,4 @@
+export type PasswordStrengthLevel = {
+  message: string;
+  color: string;
+};
