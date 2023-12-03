67d7e36641bc5b5da36efb98c90670bcc5edd0a8
petro1909 <petya-97_97@mail.ru>
2023-12-04 02:10:56 +0300

    refactor: update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
